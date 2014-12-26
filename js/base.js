d5c5e516790d45e68fea7055c0c5bc9e9b0eb35d
Rik <gitaarik@gmail.com>
2014-12-26 16:05:37 +0100

    wip

--- js/base.js
+++ js/base.js
@@ -1,27 +1,63 @@
 var React = require('react');
+var $ = require('jquery');
 
 var SearchStations = React.createClass({
+
+    getInitialState: function() {
+        return {allStations: {}};
+    },
+
+    componentDidMount: function() {
+        $.get(
+            'http://nsapi.televisionsmostpopularartinstructors.com/api/v1/stations/',
+            function(response) {
+                this.setState({allStations: response});
+            }.bind(this)
+        );
+    },
+
+    render: function() {
+        return <SearchStationsFilter data={allStations: this.state.allStations} />
+    }
+
+});
+
+var SearchStationsFilter = React.createClass({
+
+    getInitialState: function() {
+        return {
+            data: {
+                allStations: {},
+                searchResults: {}
+            }
+        };
+    },
+
     render: function() {
         return (
-            <section class="search-stations">
-                <input type="text" class="search-stations-input" />
+            <section className="search-stations">
+                <input type="text" className="search-stations-input" />
+                <SearchResults data={this.state.allStations} />
             </section>
         );
     }
+
 });
 
 var SearchResults = React.createClass({
 
     render: function() {
 
-        var resultNodes = this.props.data.map(function(result) {
-            return (
-                <li>{result.name}</li>
+        var resultNodes = [];
+
+        for (var result in this.props.data) {
+            resultNodes.push(
+                <li key={result}>{this.props.data[result].name}</li>
             );
-        });
+        }
 
         return (
-            <section class="search-results">
+            <section className="search-results">
                 {resultNodes}
             </section>
         );
@@ -30,18 +66,12 @@ var SearchResults = React.createClass({
 
 });
 
-var searchResultsData = [
-    { 'name': 'Amsterdam' },
-    { 'name': 'Haarlem' }
-];
-
 React.render(
     <div>
         <header>
             <h1>NS Vertrektijden</h1>
         </header>
         <SearchStations />
-        <SearchResults data={searchResultsData} />
     </div>,
     document.getElementById('app')
 );
