51989623aca02c360314fe9a5364db17651a369a
Rik <gitaarik@gmail.com>
2014-12-30 00:40:14 +0100

    wip

--- js/departure-times.js
+++ js/departure-times.js
@@ -6,44 +6,36 @@ var RouteHandler = Router.RouteHandler;
 var Link = Router.Link;
 var $ = require('jquery');
 var searchStations = require('./helpers/_search-stations.js');
+var stationData = require('./helpers/_station-data.js');
 
 
 var SearchStations = React.createClass({
 
     getInitialState: function() {
         return {
-            lastUpdatedTime: 0,
             stations: {},
             stationNames: {}
         };
     },
 
-    updateStations: function() {
+    componentDidMount: function() {
 
-        if (this.state.lastUpdatedTime < Date.now() - 5000) {
-
-            $.when(
-                $.get('http://nsapi.televisionsmostpopularartinstructors.com/api/v1/stationnames/'),
-                $.get('http://nsapi.televisionsmostpopularartinstructors.com/api/v1/stations/')
-            ).done(function(stationNames, stations) {
+        $.when(stationData.getStationNames(), stationData.getStations()).done(
+            function(stationNames, stations) {
 
                 if (this.isMounted()) {
                     this.setState({
-                        'lastUpdatedTime': Date.now(),
                         'stationNames': stationNames[0],
                         'stations': stations[0]
                     });
                 }
 
-            }.bind(this));
-
-        }
+            }.bind(this)
+        );
 
     },
 
     render: function() {
-
-        this.updateStations();
 
         var stationsData = {
             stations: this.state.stations,
@@ -94,14 +86,14 @@ var SearchResults = React.createClass({
 
         var resultNodes = [];
 
-        for (var result in this.props.data) {
+        for (var index in this.props.data) {
             resultNodes.push(
-                <li key={result} className="result">
+                <li key={index} className="result">
                     <Link
                         to="station" className="link"
-                        params={{stationName: this.props.data[result].code}}
+                        params={{stationName: this.props.data[index].code}}
                     >
-                        {this.props.data[result].name}
+                        {this.props.data[index].name}
                     </Link>
                 </li>
             );
@@ -136,12 +128,35 @@ var Station = React.createClass({
 
     mixins: [Router.State],
 
+    getInitialState: function() {
+        return {departureTimes: {}};
+    },
+
+    componentDidMount: function() {
+        stationData.getStation(this.getParams().stationName).done(function(departureTimes) {
+            this.setState({'departureTimes': departureTimes});
+        }.bind(this));
+    },
+
     render: function() {
+
+        var departureTimeNodes = [];
+
+        for (var index in this.state.departureTimes) {
+            departureTimeNodes.push(
+                <div key={index} className="departure">
+                    {this.state.departureTimes[index].destination}
+                </div>
+            );
+        }
+
         return (
             <div className="station">
                 Station {this.getParams().stationName}!
+                {departureTimeNodes}
             </div>
         );
+
     }
 
 });
---
+++ js/helpers/_station-data.js
@@ -0,0 +1,26 @@
+var $ = require('jquery');
+
+
+var stationData = Object.create({
+
+    initialize: function() {
+        this.baseUrl = 'http://nsapi.televisionsmostpopularartinstructors.com/api/v1/';
+    },
+
+    getStationNames: function() {
+        return $.get(this.baseUrl + 'stationnames/');
+    },
+
+    getStations: function() {
+        return $.get(this.baseUrl + 'stations/');
+    },
+
+    getStation: function(stationCode) {
+        return $.get(this.baseUrl + 'departures/' + stationCode + '/');
+    }
+
+});
+
+stationData.initialize();
+
+module.exports = stationData;
